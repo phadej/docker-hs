c3866c5762ee1cdd62e0f81fbeb7149e141b119a
Deni Bertovic <deni@denibertovic.com>
2015-01-11 19:04:29 +0100

    firt attempt at writing an interpreter for https actions

--- Network/Docker.hs
+++ Network/Docker.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 module Network.Docker where
 
 import           Control.Applicative    ((<$>), (<*>))
 import           Control.Lens
-import           Data.Aeson             (FromJSON, ToJSON, decode, eitherDecode,
-                                         toJSON)
+import           Control.Monad.Free
+import           Data.Aeson             (FromJSON, ToJSON, Value, decode,
+                                         eitherDecode, toJSON)
 import           Data.Aeson.Lens        (key, _String)
 import           Data.Aeson.TH
 import qualified Data.ByteString.Lazy   as L
@@ -19,74 +22,92 @@ import qualified Pipes.ByteString       as PB
 import qualified Pipes.HTTP             as PH
 import           Text.Printf            (printf)
 
+emptyPost = "" :: String
 
-defaultClientOpts :: DockerClientOpts
-defaultClientOpts = DockerClientOpts
-                { apiVersion = "v1.12"
-                , baseUrl = "http://127.0.0.1:3128/"
-                }
+run :: HttpRequestM r -> IO (Response L.ByteString)
+run (Free (Get url)) = get url
+run (Free (Post url body)) = post url body
 
-constructUrl :: URL -> ApiVersion -> Endpoint -> URL
+constructUrl :: URL -> ApiVersion -> String -> URL
 constructUrl url apiVersion endpoint = printf "%s%s%s" url apiVersion endpoint
 
-constructRelativeUrl url = url :: String
+-- decodeResponse :: Response L.ByteString -> a
+decodeResponse r = decode (r ^. responseBody)
 
-decodeResponse r = decode <$> (^. responseBody) <$> r
-
-getOutOfResponse k r = (^? responseBody . key k . _String) r
+getElemFromResponse k r = (^? responseBody . key k . _String) r
 
 getResponseStatusCode r = (^. responseStatus) r
 
+getEndpoint :: Endpoint -> String
+getEndpoint VersionEndpoint = "/version"
+getEndpoint ListContainersEndpoint = "/containers/json"
+getEndpoint ListImagesEndpoint = "/images/json"
+getEndpoint CreateContainerEndpoint = "/containers/create"
+getEndpoint (StartContainerEndpoint cid) = printf "/containers/%s/start" cid
+getEndpoint (StopContainerEndpoint cid) = printf "/containers/%s/stop" cid
+getEndpoint (KillContainerEndpoint cid) = printf "/containers/%s/kill" cid
+getEndpoint (RestartContainerEndpoint cid) = printf "/containers/%s/restart" cid
+getEndpoint (PauseContainerEndpoint cid) = printf "/containers/%s/pause" cid
+getEndpoint (UnpauseContainerEndpoint cid) = printf "/containers/%s/unpause" cid
+getEndpoint (ContainerLogsEndpoint cid) = printf "/containers/%s/logs" cid
+
 fullUrl :: DockerClientOpts -> Endpoint -> URL
-fullUrl clientOpts endpoint = constructUrl (baseUrl clientOpts) (apiVersion clientOpts) endpoint
+fullUrl clientOpts endpoint = constructUrl (baseUrl clientOpts) (apiVersion clientOpts) (getEndpoint endpoint)
 
-_dockerGetQuery :: Endpoint -> DockerClientOpts -> IO(Response L.ByteString)
-_dockerGetQuery endpoint clientOpts = get (fullUrl clientOpts endpoint)
+_dockerGetQuery :: DockerClientOpts -> Endpoint -> HttpRequestM Endpoint
+_dockerGetQuery clientOpts endpoint = Free (Get (fullUrl clientOpts endpoint))
 
-_dockerPostQuery :: ToJSON a => Endpoint -> DockerClientOpts -> a -> IO (Response L.ByteString)
-_dockerPostQuery endpoint clientOpts postObject = post (fullUrl clientOpts endpoint) (toJSON postObject)
+_dockerPostQuery :: ToJSON a => DockerClientOpts -> Endpoint -> a -> HttpRequestM Endpoint
+_dockerPostQuery clientOpts endpoint postObject = Free (Post (fullUrl clientOpts endpoint) (toJSON postObject))
 
-emptyPost = "" :: String
-_dockerEmptyPostQuery endpoint clientOpts = post (fullUrl clientOpts endpoint) (toJSON emptyPost)
+_dockerEmptyPostQuery :: DockerClientOpts -> Endpoint -> HttpRequestM Endpoint
+_dockerEmptyPostQuery clientOpts endpoint = Free (Post (fullUrl clientOpts endpoint) (toJSON emptyPost))
+
+getDockerVersionM :: DockerClientOpts -> HttpRequestM Endpoint
+getDockerVersionM clientOpts = _dockerGetQuery clientOpts VersionEndpoint
 
 getDockerVersion :: DockerClientOpts -> IO (Maybe DockerVersion)
-getDockerVersion = decodeResponse . _dockerGetQuery "/version"
+getDockerVersion clientOpts = decodeResponse <$> run (getDockerVersionM clientOpts)
 
-getDockerContainers :: DockerClientOpts -> IO (Maybe [DockerContainer])
-getDockerContainers = decodeResponse . _dockerGetQuery "/containers/json"
+-- getDockerContainers :: DockerClientOpts -> IO (Maybe [DockerContainer])
+-- getDockerContainers = decodeResponse . _dockerGetQuery "/containers/json"
 
-getDockerImages :: DockerClientOpts -> IO (Maybe [DockerImage])
-getDockerImages = decodeResponse . _dockerGetQuery "/images/json"
+-- getDockerImages :: DockerClientOpts -> IO (Maybe [DockerImage])
+-- getDockerImages = decodeResponse . _dockerGetQuery "/images/json"
 
-createContainer :: DockerClientOpts -> CreateContainerOpts -> IO(Maybe T.Text)
-createContainer clientOpts createOpts = getOutOfResponse "Id" <$> (_dockerPostQuery "/containers/create" clientOpts createOpts)
+-- createContainer :: DockerClientOpts -> CreateContainerOpts -> IO(Maybe T.Text)
+-- createContainer clientOpts createOpts = getElemFromResponse "Id" <$> (_dockerPostQuery "/containers/create" clientOpts createOpts)
 
-startContainer :: DockerClientOpts -> String -> StartContainerOpts -> IO(Status)
-startContainer clientOpts containerId startOpts = (^. responseStatus) <$> _dockerPostQuery (printf "/containers/%s/start" containerId) clientOpts startOpts
+-- startContainer :: DockerClientOpts -> String -> StartContainerOpts -> IO(Status)
+-- startContainer clientOpts containerId startOpts = (^. responseStatus) <$> _dockerPostQuery (printf "/containers/%s/start" containerId) clientOpts startOpts
+
+stopContainerM :: DockerClientOpts -> Endpoint -> HttpRequestM Endpoint
+stopContainerM clientOpts e = _dockerEmptyPostQuery clientOpts e
+
 
 stopContainer :: DockerClientOpts -> String -> IO (Status)
-stopContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/stop" containerId) clientOpts
+stopContainer  clientOpts cid = (^. responseStatus) <$> run (stopContainerM clientOpts (StopContainerEndpoint cid))
 
-killContainer :: DockerClientOpts -> String -> IO (Status)
-killContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/kill" containerId) clientOpts
+-- killContainer :: DockerClientOpts -> String -> IO (Status)
+-- killContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/kill" containerId) clientOpts
 
-restartContainer :: DockerClientOpts -> String -> IO (Status)
-restartContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/restart" containerId) clientOpts
+-- restartContainer :: DockerClientOpts -> String -> IO (Status)
+-- restartContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/restart" containerId) clientOpts
 
-pauseContainer :: DockerClientOpts -> String -> IO (Status)
-pauseContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/pause" containerId) clientOpts
+-- pauseContainer :: DockerClientOpts -> String -> IO (Status)
+-- pauseContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/pause" containerId) clientOpts
 
-unpauseContainer :: DockerClientOpts -> String -> IO (Status)
-unpauseContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/unpause" containerId) clientOpts
+-- unpauseContainer :: DockerClientOpts -> String -> IO (Status)
+-- unpauseContainer  clientOpts containerId = (^. responseStatus) <$> _dockerEmptyPostQuery (printf "/containers/%s/unpause" containerId) clientOpts
 
-getContainerLogsStream :: DockerClientOpts -> String -> IO ()
-getContainerLogsStream  clientOpts containerId = do
-                req <- PH.parseUrl (fullUrl clientOpts url)
-                let req' =  req {PH.method = "GET"}
-                PH.withManager PH.defaultManagerSettings $ \m  -> PH.withHTTP req' m  $ \resp -> runEffect $ PH.responseBody resp >-> PB.stdout
-        where url = (printf "/containers/%s/logs?stdout=1&stderr=1&follow=1" containerId)
+-- getContainerLogsStream :: DockerClientOpts -> String -> IO ()
+-- getContainerLogsStream  clientOpts containerId = do
+--                 req <- PH.parseUrl (fullUrl clientOpts url)
+--                 let req' =  req {PH.method = "GET"}
+--                 PH.withManager PH.defaultManagerSettings $ \m  -> PH.withHTTP req' m  $ \resp -> runEffect $ PH.responseBody resp >-> PB.stdout
+--         where url = (printf "/containers/%s/logs?stdout=1&stderr=1&follow=1" containerId)
 
-getContainerLogs :: DockerClientOpts -> String -> IO (L.ByteString)
-getContainerLogs  clientOpts containerId = (^. responseBody) <$> _dockerGetQuery url clientOpts
-        where url = (printf "/containers/%s/logs?stdout=1&stderr=1" containerId)
+-- getContainerLogs :: DockerClientOpts -> String -> IO (L.ByteString)
+-- getContainerLogs  clientOpts containerId = (^. responseBody) <$> _dockerGetQuery url clientOpts
+--         where url = (printf "/containers/%s/logs?stdout=1&stderr=1" containerId)
 
